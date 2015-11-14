0373739fb7518cd08c125d15fe789cd9bc60f109
eldyvoon <eldy8888@gmail.com>
2015-11-14 20:34:37 +0800

    added handlebars

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var exphbs = require('express3-handlebars')
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/',function(req,res){
+	res.render('index');
+});
+
+app.use('/public', express.static('public'));
+
+var port = Number(process.env.PORT || 3000);
+app.listen(port);
