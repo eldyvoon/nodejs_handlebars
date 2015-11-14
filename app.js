e91afd2fcf3a7e252deded95f5515b98e8399ff0
eldyvoon <eldy8888@gmail.com>
2015-11-14 23:01:07 +0800

    fixed handlebars routing

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/',function(req,res){
-	res.render('index');
+	res.render('index',{
+		name:"Eldy"
+	});
 });
 
 app.use('/public', express.static('public'));
